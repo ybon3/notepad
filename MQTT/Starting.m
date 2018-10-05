53a6fc1c9b6724d0408af57ac989c733eda90395
Dante Lee <lee.ybon@gmail.com>
2018-10-05 14:51:14 +0800

    Create Starting.m

---
+++ MQTT/Starting.m
@@ -0,0 +1,10 @@
+## MQTT教學（一）：認識MQTT
+https://swf.com.tw/?p=1002
+
+
+## MQTT(一)簡介 
+http://blog.maxkit.com.tw/2014/01/mqtt.html
+
+
+## 5 Protocols For Event-Driven API Architectures
+https://nordicapis.com/5-protocols-for-event-driven-api-architectures/
