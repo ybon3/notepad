b6b5847d47049c7b04317a937280a013444c6a5f
Dante Lee <lee.ybon@gmail.com>
2020-02-07 09:29:45 +0800

    Add files via upload

---
+++ Java/AesTest.java
@@ -0,0 +1,82 @@
+package com.nkg.crypto;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AesTest {
+
+	public static final String ALGORITHM = "AES";
+	public static final String SECRET = "qwer1234";
+
+	public static void main(String[] args) throws Exception {
+
+		SecretKeySpec secretKeySpec = secretKeyWithSha256(SECRET, ALGORITHM);
+
+		//ENCRYPT
+		String plainText = "BT2020020001233";
+
+		byte[] enc = encryptAES(secretKeySpec, plainText.getBytes("UTF-8"));
+
+		System.out.println(Base64.getEncoder().encodeToString(enc));
+
+
+		//DECRYPT
+		String cipherText = "lh52yxDlAurSyaW+R/Std2olkXt4di/OOqThGB+7IQ==";
+
+		byte[] data = Base64.getDecoder().decode(cipherText.getBytes("UTF-8"));
+
+		System.out.println(new String(decrypt(secretKeySpec, data), "UTF-8"));
+	}
+
+	public static byte[] encryptAES(SecretKeySpec secretKey, byte[] plainText) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		byte[] cipherText = cipher.doFinal(plainText);
+
+		byte[] iv = cipher.getIV();
+		byte[] des = new byte[iv.length + cipherText.length];
+		System.arraycopy(iv, 0, des, 0, iv.length);
+		System.arraycopy(cipherText, 0, des, iv.length, cipherText.length);
+		return des;
+	}
+
+	public static byte[] decrypt(SecretKey secretKey, byte[] cipherText) throws Exception {
+		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+
+		int blockSize = cipher.getBlockSize();
+		byte[] iv = slice(cipherText, 0, blockSize);
+		byte[] payload = slice(cipherText, blockSize, cipherText.length);
+		cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+
+		return cipher.doFinal(payload);
+	}
+
+	public static SecretKeySpec secretKeyWithSha256(String secret, String algorithm)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(secret.getBytes("utf-8"));
+		int maxAllowedKeyLength = Cipher.getMaxAllowedKeyLength(algorithm) / 8;
+		byte[] raw = slice(md.digest(), 0, maxAllowedKeyLength);
+		return new SecretKeySpec(raw, algorithm);
+	}
+
+	public static byte[] slice(byte[] src, int s, int e) {
+		if (s >= src.length) {
+			return new byte[0];
+		}
+
+		if (e > src.length) {
+			e = src.length;
+		}
+
+		return Arrays.copyOfRange(src, s, e);
+	}
+}
---
+++ Java/AesTest2.java
@@ -0,0 +1,83 @@
+package com.nkg.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+/**
+ * Stanley 提供的版本
+ */
+public class AesTest2 {
+
+	private static final String KEY_VALUE = "BU2020010000211";
+
+	public static void main(String args[]) throws Exception {
+		try {
+			String privateKey = "qwer1234";
+			MessageDigest messageDigest = null;
+			try {
+				messageDigest = MessageDigest.getInstance("SHA-256");
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			int maxLength = 16;
+			System.out.println("maxLength: " + maxLength);
+			messageDigest.update(privateKey.getBytes("UTF-8"));
+			byte[] raw = new byte[maxLength];
+			byte[] md = messageDigest.digest();
+			for (int ii = 0; ii < maxLength; ii++) {
+				raw[ii] = md[ii];
+			}
+			SecretKeySpec secretKey = new SecretKeySpec(raw, "AES");
+
+			String encstr = encryptAES(KEY_VALUE, secretKey);
+			System.out.println("Encrypt String: " + encstr);
+			encstr = "lh52yxDlAurSyaW+R/Std2olkXt4di/OOqThGB+7IQ==";
+			String decstr = decryptAES(encstr, secretKey);
+			System.out.println("Decrypt String: " + decstr);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String encryptAES(String oristr, SecretKeySpec secretKey) {
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			byte[] byteCipherText = cipher.doFinal(oristr.getBytes("UTF-8"));
+			byte[] des  = new byte[cipher.getIV().length + byteCipherText.length];
+			System.arraycopy(cipher.getIV(), 0, des, 0, cipher.getIV().length);
+			System.arraycopy(byteCipherText, 0, des, cipher.getIV().length, byteCipherText.length);
+			String res = Base64.getEncoder().encodeToString(des);
+			return res;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static String decryptAES(String encstr, SecretKeySpec secretKey) {
+		try {
+			Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+			byte[] value = Base64.getDecoder().decode(encstr);
+			byte[] iv = new byte[cipher.getBlockSize()];
+			for (int ii = 0; ii < cipher.getBlockSize(); ii++) {
+				iv[ii] = value[ii];
+			}
+			byte[] payload = new byte[value.length - cipher.getBlockSize()];
+			for (int ii = 0; ii < payload.length; ii++) {
+				payload[ii] = value[cipher.getBlockSize() + ii];
+			}
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			String strDecryptedText = new String(cipher.doFinal(payload));
+			return strDecryptedText;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
